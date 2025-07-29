7c69c617fbfd9b91765d68cc3f50bdd3d085b5ba
techbrobolu <techbrobolu@gmail.com>
2025-07-29 17:58:11 +0100

    setup error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,11 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc  Auth user/set token
 // route  POST /api/users/auth
 // @access Public
-const authUser = async (req, res) => {
-    res.status(200).json({message: 'Auth User'})
-};
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Auth User'});
+});
 
 export{
     authUser
