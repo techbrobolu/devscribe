ca7252c0d5ba2c2bcc00e29d668f4ccb594add55
techbrobolu <techbrobolu@gmail.com>
2025-07-30 07:10:38 +0100

    setup profile routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,6 +7,42 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Auth User'});
 });
 
+
+// @desc   Register a new user
+// route   POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Register User'});
+});
+
+
+// @desc  Logout a user
+// route  POST /api/users/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Logout User'});
+});
+
+
+// @desc  Get user profile
+// route  GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'User Profile'});
+});
+
+
+// @desc  Update user profile
+// route  PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Update User Profile'});
+});
+
 export{
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 };
